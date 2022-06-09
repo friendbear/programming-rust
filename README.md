c0b38521070867bff5a2c0ba7fbcc62351dc2b25
T Kumagai <friendbear@users.noreply.github.com>
2022-06-09 11:15:10 +0900

    programming rust chapter2 tour2

---
+++ README.md
@@ -0,0 +1,7 @@
+# Programming Rust
+
+## Oreilly Book
+
+[link](https://www.oreilly.co.jp/books/9784873118550/)
+
+![img](book-978-4-87311-855-0.jpeg)
---
+++ chapter1/tour24/src/main.rs
@@ -0,0 +1,20 @@
+use std::io::Write;
+use std::str::FromStr;
+
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    println!("Debug {:?}", &numbers);
+
+}

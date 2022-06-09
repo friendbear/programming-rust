3063742f25a606a216f1c07eb8f0df1a957979ad
T Kumagai <friendbear@users.noreply.github.com>
2022-06-09 12:21:41 +0900

    FromStr u64::from_str(s: &str)

--- chapter1/tour24/src/main.rs
+++ chapter1/tour24/src/main.rs
@@ -15,6 +15,23 @@ fn main() {
         std::process::exit(1);
     }
 
-    println!("Debug {:?}", &numbers);
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }

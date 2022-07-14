20470fcf4e32e716708f6ce8e52b706698670343
T Kumagai <friendbear@users.noreply.github.com>
2022-07-14 14:19:00 +0900

    downgrade crate versions

--- chapter1/tour25-iron-gcd/src/main.rs
+++ chapter1/tour25-iron-gcd/src/main.rs
@@ -1,14 +1,22 @@
 extern crate iron;
 extern crate router;
+extern crate urlencoded;
 use iron::prelude::*;
 use iron::mime::*;
 use iron::status;
+use router::Router;
+use std::str::FromStr;
+use urlencoded::UrlEncodedBody;
 
 fn main() {
 
+    let mut router = Router::new();
 
+    router.get("/", get_form, "root");
+    router.post("/gcd", post_gcd, "gcd");
+    
     println!("Serving on http://localhost:3000...");
-    Iron::new(get_form).http("localhost:3000").unwrap();
+    Iron::new(router).http("localhost:3000").unwrap();
 }
 fn get_form(_request: &mut Request) -> IronResult<Response> {
     let mut response = Response::new();
@@ -23,4 +31,39 @@ fn get_form(_request: &mut Request) -> IronResult<Response> {
         </form>
         "#);
     Ok(response)
+}
+
+fn post_gcd(request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    let form_data = match request.get_ref::<UrlEncodedBody>() {
+        Err(e) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Error parsing form data: {:?}\n", e));
+            return Ok(response);
+        }
+        Ok(map) => map
+    };
+
+    let unparsed_numbers = match form_data.get("n") {
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("form data has no 'n' parameter\n"));
+            return Ok(response);
+        }
+        Some(nums) => nums
+    };
+
+    let mut numbers = Vec::new();
+    for unparsed in unparsed_numbers {
+        match u64::from_str(unparsed) {
+            Err(_) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(format!("Value for 'n' parameter not a number: {:?}\n", unparsed));
+            }
+            Ok(n) => { numbers.push(n)}
+        }
+    }
+
+    todo!()
 }

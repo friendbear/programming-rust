d261aead7f70da0ee6db2896c2db7488c856f9eb
T Kumagai <friendbear@users.noreply.github.com>
2022-07-26 11:25:10 +0900

    cargo run mandel.png 2550x1025 -1.20,0.35 -1,0.20

--- chapter2/mandelbrot/src/main.rs
+++ chapter2/mandelbrot/src/main.rs
@@ -2,9 +2,6 @@ extern crate num;
 
 use num::Complex;
 
-fn main() {
-    println!("Hello, world!");
-}
 
 fn complex_squre_add_loop(c: Complex<f64>) {
     let mut z = Complex { re: 0.0, im: 0.0 };
@@ -119,4 +116,36 @@ fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
                     ColorType::Gray(8))?;
 
     Ok(())
+}
+
+use std::io::Write;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(),
+            "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRLIGHT")
+        .unwrap();
+        writeln!(std::io::stderr(),
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0])
+        .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair::<usize>(&args[2], 'x')
+            .expect("error parsing image dimensions");
+    
+    let upper_left = parse_complex(&args[3])
+            .expect("error parsing upper left conrner point");
+    let lower_right = parse_complex(&args[4])
+            .expect("error parsing lower right conrner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+            .expect("error writing PNG file");
+
 }

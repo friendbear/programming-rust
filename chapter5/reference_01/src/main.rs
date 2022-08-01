9930e41f41dd469d6629fcc003714b06d29dc1bb
T Kumagai <friendbear@users.noreply.github.com>
2022-08-01 16:18:14 +0900

    lifetime parameter &self

--- chapter5/reference_01/src/main.rs
+++ chapter5/reference_01/src/main.rs
@@ -124,3 +124,17 @@ fn test_lifetime_two(){
         assert_eq!(f(&a, &b), &10);
     }
 }
+
+struct StringTable {
+    elements: Vec<String>,
+}
+impl StringTable {
+    fn find_by_prefix(&self, prefix: &str) -> Option<&String> {
+        for i in 0..self.elements.len() {
+            if self.elements[i].starts_with(prefix) {
+                return Some(&self.elements[i]);
+            }
+        }
+        None
+    }
+}

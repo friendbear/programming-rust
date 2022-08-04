8c484ac84f381bfdcfec034fec799b5a0c1e4629
T Kumagai <friendbear@users.noreply.github.com>
2022-08-04 11:38:49 +0900

    file move fn

--- chapter7/result/src/main.rs
+++ chapter7/result/src/main.rs
@@ -1,9 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::path::Path;
+use std::io;
+use std::fs;
+
 use std::error::Error;
 use std::io::{Write, stderr};
-
+/* 
 fn print_error(err: &dyn Error) {
     writeln!(stderr(), "error {}", err);
     while let Some(source) = Error::source(err) {
@@ -11,4 +12,28 @@ fn print_error(err: &dyn Error) {
         writeln!(stderr(), "caused by: {:?}", Error::source(err));
         err = Error::source(err);
     }
+}
+*/
+
+fn main() -> std::io::Result<()> {
+    for entry in fs::read_dir(".")? {
+        let dir = entry?;
+        println!("{:?}", dir.path());
+    }
+
+//    let src = fs::read_dir(".")?.last()?;
+    let src = Path::new(".");
+    let dst = Path::new("/tmp");
+    move_all(src, dst)?;
+
+    Ok(())
+}
+fn move_all(src: &Path, dst: &Path) -> io::Result<()> {
+    for entry_result in src.read_dir().expect("read dir call failed.") {
+        if let Ok(entry) = entry_result {
+            let dst_file = dst.join(entry.file_name());
+            fs::rename(entry.path(), dst_file)?;
+        }
+    }
+    Ok(())
 }

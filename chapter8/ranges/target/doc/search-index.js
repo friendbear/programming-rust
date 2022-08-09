9b54e3b61021b8d3f2a38ae19559b908ef1b646c
T Kumagai <friendbear@users.noreply.github.com>
2022-08-09 10:36:15 +0900

    ドクテスト

---
+++ chapter8/ranges/target/doc/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"ranges":{"doc":"","t":[5],"n":["overlap"],"q":["ranges"],"d":["Return true if two ranges overlap."],"i":[0],"f":[[[["range",3,[["usize",0]]],["range",3,[["usize",0]]]],["bool",0]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ chapter8/ranges/target/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["ranges"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();

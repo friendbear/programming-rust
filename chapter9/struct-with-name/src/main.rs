6866aa3e8169d4a03a91a9d3d4087d1d5a4d4473
T Kumagai <friendbear@users.noreply.github.com>
2022-08-20 10:48:00 +0900

    struct with name

---
+++ chapter9/struct-with-name/src/main.rs
@@ -0,0 +1,44 @@
+// 名前付きフィールド構造体
+#[derive(Debug, Clone)]
+struct Broom{
+    name: String, 
+    height: u32,
+    health: u32,
+    position:(f32, f32, f32),
+    intent: BroomIntent
+}
+
+#[derive(Copy, Clone, Debug)]
+enum BroomIntent { FetchWater, DumpWater }
+fn main() {
+    let hokey = Broom {
+        name: "Hokey".to_string(),
+        height: 60,
+        health: 100,
+        position: (100.0, 200.0, 0.0),
+        intent: BroomIntent::FetchWater
+    };
+
+    let (hokey1, hokey2) = chop(hokey);
+    dbg!(&hokey1);
+    assert_eq!(hokey1.name, "Hokey I");
+    assert_eq!(hokey1.health, 100);
+    
+
+    dbg!(&hokey2);
+    assert_eq!(hokey2.name, "Hokey II");
+    assert_eq!(hokey2.health, 100);
+
+}
+
+fn chop(b: Broom) -> (Broom, Broom) {
+
+    let mut broom1 = Broom { height: b.height/2, .. b};
+
+    // Stringはコピーではないので`broom1`は`b`の名前の所有権を得る
+    let mut broom2 = Broom{ name: broom1.name.clone(), .. broom1};
+    broom1.name.push_str(" I");
+    broom2.name.push_str(" II");
+
+    (broom1, broom2)
+}

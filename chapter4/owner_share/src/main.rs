11d99a1ebed525386b57a5162ba927aff192978a
T Kumagai <4956799+friendbear@users.noreply.github.com>
2024-05-29 13:52:07 +0900

    Add test Rc::strong_count()

--- chapter4/owner_share/src/main.rs
+++ chapter4/owner_share/src/main.rs
@@ -9,10 +9,19 @@ fn main() {
 #[test]
 fn test_owner_share() {
     let s: Rc<String> = Rc::new("shirataki".to_string());
-    let t: Rc<String> = s.clone();
-    let u: Rc<String> = s.clone();
+    {
 
+        let t = s.clone();
+        {
+
+            let u = s.clone();
+            println!("{} are quite chewy, almost bouncy, but lack flavor", u);
+            assert_eq!(Rc::strong_count(&s), 3);
+        }
+        assert_eq!(t.find("taki"), Some(5));
+        assert_eq!(Rc::strong_count(&s), 2);
+
+    }
     assert!(s.contains("shira"));
-    assert_eq!(t.find("taki"), Some(5));
-    println!("{} are quite chewy, almost bouncy, but lack flavor", u);
+    assert_eq!(Rc::strong_count(&s), 1);
 }

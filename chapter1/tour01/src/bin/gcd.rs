af2cb6447717bdeb3271d7de3e6855e218bbff17
T Kumagai <friendbear@users.noreply.github.com>
2024-07-07 17:02:19 +0900

    fix: warning
    ➜ cargo -V
    cargo 1.76.0 (c84b36747 2024-01-18)
    
    ➜ cargo clippy -V
    clippy 0.1.76 (07dca48 2024-02-04)

--- chapter1/tour01/src/bin/gcd.rs
+++ chapter1/tour01/src/bin/gcd.rs
@@ -4,20 +4,37 @@ struct User {
     lastname: String,
 }
 
+#[warn(clippy::vec_init_then_push)]
 fn main() {
-
-    let mut v = vec![];
-        v.push(User{ firstname: "T".to_owned(),  lastname: "Kumagai".to_owned()});
-        v.push(User{ firstname: "S".to_owned(),  lastname: "Kumagai".to_owned()});
+    // let mut v = vec![];
+    let v = vec![User {
+            firstname: "T".to_owned(),
+            lastname: "Kumagai".to_owned(),
+        },
+        User {
+            firstname: "S".to_owned(),
+            lastname: "Kumagai".to_owned(),
+        }
+    ];
 
     for u in v {
         const CHARSET: &str = "abcdefghijklmnopqrstuvwxyz1234567890_!?=<>";
         let vec = CHARSET.chars().collect::<Vec<_>>();
-        let n = u.firstname.chars().collect::<Vec<_>>();
-        println!("{:?}", n);
 
-        if u.firstname.contains("S") {
+        // TODO: if for contains char
+        #[warn(clippy::let_unit_value)]
+        let is_charset_contain = vec.iter().for_each(|c| {
+            c.eq(&'0').then(|| Some(true).or(Some(false)));
+        });
+        dbg!(is_charset_contain);
 
+        let v_first_name = u.firstname.chars().collect::<Vec<_>>();
+        println!("{:?}", v_first_name);
+        let v_last_name = u.lastname.chars().collect::<Vec<_>>();
+        println!("{:?}", v_last_name);
+
+        #[warn(clippy::single_char_pattern)]
+        if u.firstname.contains('S') {
             println!("{:?}", u);
         }
     }
@@ -25,15 +42,17 @@ fn main() {
     println!("{}", gcd(14, 15));
 }
 
-fn gcd(mut n: u64, mut m: u64) -> u64  {
+fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
         if m < n {
-            let t = m;
-            m = n;
-            n = t;
+            // let t = m;
+            #[warn(clippy::manual_swap)]
+            std::mem::swap(&mut n, &mut m);
+            // m = n;
+            // n = t;
         }
-        m = m % n;
+        m %= n;
     }
     n
 }
@@ -42,7 +61,5 @@ fn gcd(mut n: u64, mut m: u64) -> u64  {
 fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
 
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
-                   3 * 7 * 11 * 13 * 19),
-               3 * 11);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }

2e7a8303d75d31477556d15b59ddadb0d5f7491f
T Kumagai <4956799+friendbear@users.noreply.github.com>
2024-08-03 16:04:26 +0900

    add doctest.

--- chapter8/module_fern_sim/src/lib.rs
+++ chapter8/module_fern_sim/src/lib.rs
@@ -1,3 +1,14 @@
+//! Simulate the growth of a fern over time.
+//!
+/// ```
+///    use fern_sim::{run_simulation, Fern};
+///     let mut fern = Fern {
+///         size: 1.0,
+///        growth_rate: 0.01,
+///     };
+///     run_simulation(&mut fern, 100);
+///     assert!(fern.size > 1.0);
+/// ```
 pub struct Fern {
     pub size: f64,
     pub growth_rate: f64, // Growth rate for each leaf

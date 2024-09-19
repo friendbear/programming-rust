e8e0ba03fdfe464b46bec9905cc826f0c63b5a87
T Kumagai <4956799+friendbear@users.noreply.github.com>
2024-09-19 10:57:10 +0900

    Drop trait.

--- chapter13/src/lib.rs
+++ chapter13/src/lib.rs
@@ -1,20 +1,6 @@
 use std::ops::Deref;
 use std::ops::DerefMut;
-
-pub struct Application {
-    pub name: String,
-    pub nickname: Vec<String>,
-}
-
-impl std::ops::Drop for Application {
-    fn drop(&mut self) {
-        print!("Dropping {}", self.name);
-        if !self.nickname.is_empty() {
-            print!("(AKA{})", self.nickname.join(", "))
-        }
-        println!();
-    }
-}
+pub mod drop;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
